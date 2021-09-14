07534ade461a76fe10e5f14779c12f4906cc1ff3
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-09-14 19:58:11 +0200

    add elements

---
+++ src/pages/Home.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import DeleteIcon from '@material-ui/icons/Delete';
+import SaveIcon from '@material-ui/icons/Save';
+import { orange } from '@material-ui/core/colors';
+import {
+  ButtonGroup,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  TextField,
+  makeStyles,
+  ThemeProvider,
+  createTheme
+} from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B, #FF8E53)',
+    border: 0,
+    marginBottom: 15,
+    borderRadius: 15,
+    color: 'white',
+    padding: '5px 30px'
+  }
+});
+
+const theme = createTheme({
+  pallete: {
+    primary: {
+      main: orange[400]
+    }, secondar: {
+      main: orange[400]
+    }
+  }
+});
+
+const ButtonStyled = () => {
+  const classes = useStyles()
+  return <Button className={classes.root}>Test Styled Button</Button>
+}
+
+const CheckboxExample = () => {
+  const [checked, setChecked] = useState(true)
+  return (
+    <FormControlLabel
+      control={<Checkbox
+        checked={checked}
+        icon={<SaveIcon />}
+        checkedIcon={<DeleteIcon />}
+        onChange={(event) => setChecked(event.target.checked)}
+        inputProps={{
+          'aria-label': 'secondary-checkbox'
+        }}
+      />}
+      label='Testing Checkbox'
+    />
+  )
+}
+
+function Home() {
+  return (
+    <ThemeProvider theme={theme}>
+      <div className='App'>
+        <header className='App-header'>
+          <ButtonStyled />
+          <TextField
+            variant='filled'
+            color='secondary'
+            type='email'
+            label='SU Email'
+            value='sample@sun.ac.za'
+          />
+          <CheckboxExample />
+          <h1>Welcome to Home!</h1>
+          <ButtonGroup
+            variant='contained'
+            color='primary'
+          >
+            <Button
+              startIcon={<SaveIcon />}
+            >
+              Save
+            </Button>
+            <Button
+              startIcon={<DeleteIcon />}
+              color='secondary'
+            >
+              Discard
+            </Button>
+          </ButtonGroup>
+        </header>
+      </div>
+    </ThemeProvider>
+  )
+}
+
+export default Home;
